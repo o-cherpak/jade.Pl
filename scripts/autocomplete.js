c9c7419c8601b5d5b01db76a03f0e0218935f1ef
Oleksandr Cherpak <cherpak.oleksandr.wsei@gmail.com>
2025-05-19 10:39:03 +0200

    add questions page

--- scripts/autocomplete.js
+++ scripts/autocomplete.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   fetch("./src/cities.json")
     .then((response) => response.json())
     .then((data) => {
-      const cities = data.map((item) => item);
+      const cities = data.map((city) => city.name);
 
       $("#cityInputA").autocomplete({ source: cities });
       $("#cityInputB").autocomplete({ source: cities });
